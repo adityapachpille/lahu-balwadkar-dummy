9af59faa97f6026f450b9a7fbb4fca78c66d5a59
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2026-01-12 00:47:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -273,7 +273,7 @@ const SoundCheck = () => {
 
   <h1 className="text-center text-xl font-bold mb-4 mt-6">
         <span className="text-red-600">कमळ </span> या निशाणी समोरील बटन दाबून{" "}
-        <span className="text-red-600">भारतीय जनता पक्षाच्या .</span>
+        <span className="text-red-600">भारतीय जनता पक्षाच्या</span>
         चारही उमेदवारांना प्रचंड बहुमतांनी विजयी करा
       </h1>
 
